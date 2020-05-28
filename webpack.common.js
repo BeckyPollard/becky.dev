92fc045c72d8f89a3a38f91db5b87d5d30a42d14
BeckyPollard <hello@beckypollard.com>
2020-05-27 21:40:07 -0400

    Updating quickstart

--- webpack.common.js
+++ webpack.common.js
@@ -8,8 +8,22 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/main.html',
+      template: './src/index.html',
+      filename: 'index.html',
+      favicon: './src/favicon.ico',
     }),
+    new HtmlWebpackPlugin({
+      template: './src/about.html',
+      filename: 'about.html',
+      favicon: './src/favicon.ico',
+    }),
+    // ADD FOR EACH NEW HTML PAGE
+    // new HtmlWebpackPlugin({
+    //   template: './src/name.html',
+    //   filename: 'name.html',
+    //   favicon: './src/favicon.ico',
+    // }),
+    // TODO: better auto way of handling html pages?
   ],
   module: {
     rules: [
@@ -20,7 +34,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(svg|png|jpg|jpe)$/,
+        test: /\.(svg|png|jpg|jpeg)$/,
         use: {
           loader: 'file-loader', //require assets linked in html
           options: {
